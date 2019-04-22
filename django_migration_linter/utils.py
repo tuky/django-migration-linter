84c13029ca0fce9650669333634899387d36c709
David Wobrock <david.wobrock@gmail.com>
2019-04-22 09:17:18 +0200

    wip

--- django_migration_linter/utils.py
+++ django_migration_linter/utils.py
@@ -56,4 +56,8 @@ def get_migration_abspath(app_label, migration_name):
     module_name, _ = MigrationLoader.migrations_module(app_label)
     migration_path = "{}.{}".format(module_name, migration_name)
     migration_module = import_module(migration_path)
-    return migration_module.__file__
+
+    migration_file = migration_module.__file__
+    if migration_file.endswith(".pyc"):
+        migration_file = migration_file[:-1]
+    return migration_file
